7a7bf162c070593790e3250e30783e02637bc9de
manojhulk/firstproject <manoj.kumar.munnangi@netcracker.com>
2022-10-19 13:23:24 +0530

    Create The Test class

---
+++ src/com/netcracker/mytask/ExampleAbstraction.java
@@ -0,0 +1,17 @@
+package com.netcracker.mytask;
+
+public class ExampleAbstraction {
+    private String name;
+
+    public ExampleAbstraction(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
